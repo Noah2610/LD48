166bad2ee37857712119d2409ed3894d58c76b59
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-04-25 20:29:57 +0200

    Write HandleZoneSwitch system, add ShouldLoadNextZone resource

--- src/systems/handle_zone_switch.rs
+++ src/systems/handle_zone_switch.rs
@@ -1,12 +1,48 @@
 use super::system_prelude::*;
-use deathframe::core::geo::prelude::Rect;
+use deathframe::physics::query;
+use query::prelude::{FindQuery, Query};
 
 #[derive(Default)]
 pub struct HandleZoneSwitch;
 
 impl<'a> System<'a> for HandleZoneSwitch {
-    type SystemData = (WriteExpect<'a, ZonesManager>,);
+    type SystemData = (
+        WriteExpect<'a, ShouldLoadNextZone>,
+        ReadStorage<'a, Player>,
+        ReadStorage<'a, Collider<CollisionTag>>,
+    );
 
-    fn run(&mut self, (mut zones_manager,): Self::SystemData) {
+    fn run(
+        &mut self,
+        (
+            mut should_load_next_zone,
+            player_store,
+            collider_store,
+        ): Self::SystemData,
+    ) {
+        if !should_load_next_zone.0 {
+            let should_switch_zone = {
+                (&player_store, &collider_store).join().any(
+                    |(_, player_collider)| {
+                        let query_exp = {
+                            use query::exp::prelude_variants::*;
+                            And(vec![
+                                IsState(Enter),
+                                IsTag(CollisionTag::Portal),
+                            ])
+                        };
+                        player_collider
+                            .query::<FindQuery<CollisionTag>>()
+                            .exp(&query_exp)
+                            .run()
+                            .is_some()
+                    },
+                )
+            };
+
+            if should_switch_zone {
+                should_load_next_zone.0 = true;
+            }
+        }
     }
 }
