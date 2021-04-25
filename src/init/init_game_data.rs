1b6e55be57a2e0e62c972015f083eab7dfe7efa7
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-04-25 22:42:05 +0200

    Register ZoneTransition dispatcher

--- src/init/init_game_data.rs
+++ src/init/init_game_data.rs
@@ -37,6 +37,7 @@ pub(super) fn build_game_data<'a, 'b>(
         .dispatcher(DispatcherId::MainMenu)?
         .dispatcher(DispatcherId::Ingame)?
         .dispatcher(DispatcherId::Paused)?
+        .dispatcher(DispatcherId::ZoneTransition)?
         .with_core_bundle(FpsCounterBundle)?
         .with_core_bundle(transform_bundle)?
         .with_core_bundle(rendering_bundle)?
