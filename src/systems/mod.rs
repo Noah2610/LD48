98c5cbc604ea4ee28bf284729580fdb5a4017c16
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-04-25 18:52:56 +0200

    Rename HandleZoneSwitch system to ConfineCameraToFinalSegment

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,12 +1,12 @@
+mod confine_camera_to_final_segment;
 mod control_player;
 mod handle_segment_loading;
-mod handle_zone_switch;
 mod update_on_lane;
 
 pub mod prelude {
+    pub use super::confine_camera_to_final_segment::ConfineCameraToFinalSegment;
     pub use super::control_player::ControlPlayer;
     pub use super::handle_segment_loading::HandleSegmentLoading;
-    pub use super::handle_zone_switch::HandleZoneSwitch;
     pub use super::update_on_lane::UpdateOnLane;
     pub use deathframe::systems::prelude::*;
 }
