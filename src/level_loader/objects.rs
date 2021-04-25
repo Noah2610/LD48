f28d0073c0d232654d66d485aab78f46f5ff034b
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-04-25 17:11:32 +0200

    Update deathframe, camera follows player only on Y axis

--- src/level_loader/objects.rs
+++ src/level_loader/objects.rs
@@ -8,7 +8,7 @@ use crate::settings::prelude::*;
 use crate::settings::zones_settings::SegmentId;
 use amethyst::ecs::{Builder, Entity, World, WorldExt};
 use deathframe::amethyst;
-use deathframe::core::geo::prelude::Rect;
+use deathframe::core::geo::prelude::{Axis, Rect};
 use deathframe::resources::SpriteSheetHandles;
 use std::path::PathBuf;
 
@@ -143,7 +143,7 @@ pub fn build_camera(
 
     world
         .create_entity()
-        .with(Follow::new(player))
+        .with(Follow::new(player).with_only_axis(Axis::Y))
         // .with(Confined::from(Rect {
         //     top:    level_size.h,
         //     bottom: 0.0,
