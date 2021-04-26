c6fde35c870f874d36f5e050372388efe03294eb
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-04-26 20:57:36 +0200

    Add z to player settings

--- src/settings/player_settings.rs
+++ src/settings/player_settings.rs
@@ -5,5 +5,6 @@ use super::entity_components::EntityComponents;
 #[derive(Deserialize, Clone)]
 #[serde(deny_unknown_fields)]
 pub struct PlayerSettings {
+    pub z:          f32,
     pub components: EntityComponents,
 }
