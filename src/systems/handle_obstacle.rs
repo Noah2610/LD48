f18d604426d62fabf34d2ce4724e5981b498bdfb
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-05-02 05:33:25 +0200

    Stop player velocity on obstacle collision

--- src/systems/handle_obstacle.rs
+++ src/systems/handle_obstacle.rs
@@ -14,6 +14,7 @@ impl<'a> System<'a> for HandleObstacle {
         ReadStorage<'a, Obstacle>,
         ReadStorage<'a, Collider<CollisionTag>>,
         WriteStorage<'a, AnimationsContainer<AnimationKey>>,
+        WriteStorage<'a, Velocity>,
     );
 
     fn run(
@@ -26,42 +27,49 @@ impl<'a> System<'a> for HandleObstacle {
             obstacle_store,
             collider_store,
             mut animations_store,
+            mut velocity_store,
         ): Self::SystemData,
     ) {
         if !game_over.0 {
-            let did_player_get_hit =
-                (&player_store, &collider_store, &mut animations_store)
-                    .join()
-                    .find_map(|(_, collider, animations)| {
-                        let query_exp = {
-                            use query::exp::prelude_variants::*;
-                            And(vec![
-                                IsState(Steady),
-                                IsTag(CollisionTag::Obstacle),
-                            ])
-                        };
-                        let collision = collider
-                            .query::<FindQuery<CollisionTag>>()
-                            .exp(&query_exp)
-                            .run();
-                        if let Some(collision) = collision {
-                            let do_collide = (&entities, &obstacle_store)
-                                .join()
-                                .any(|(obstacle_entity, _)| {
-                                    obstacle_entity.id() == collision.id
-                                });
-                            if do_collide {
-                                Some(animations)
-                            } else {
-                                None
-                            }
+            let did_player_get_hit = (
+                &player_store,
+                &collider_store,
+                &mut animations_store,
+                &mut velocity_store,
+            )
+                .join()
+                .find_map(|(_, collider, animations, velocity)| {
+                    let query_exp = {
+                        use query::exp::prelude_variants::*;
+                        And(vec![
+                            IsState(Steady),
+                            IsTag(CollisionTag::Obstacle),
+                        ])
+                    };
+                    let collision = collider
+                        .query::<FindQuery<CollisionTag>>()
+                        .exp(&query_exp)
+                        .run();
+                    if let Some(collision) = collision {
+                        let do_collide = (&entities, &obstacle_store)
+                            .join()
+                            .any(|(obstacle_entity, _)| {
+                                obstacle_entity.id() == collision.id
+                            });
+                        if do_collide {
+                            Some((animations, velocity))
                         } else {
                             None
                         }
-                    });
+                    } else {
+                        None
+                    }
+                });
 
-            if let Some(animations) = did_player_get_hit {
+            if let Some((animations, velocity)) = did_player_get_hit {
                 game_over.0 = true;
+                velocity.x = 0.0;
+                velocity.y = 0.0;
                 let _ = animations.play(AnimationKey::GameOver);
                 sound_player.add_action(SoundAction::Play(SoundKey::GameOver));
             }
