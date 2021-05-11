da25d4a95e2ece56616f535f662afbf9b46c4414
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-05-11 21:45:57 +0200

    Don't cache last selected zone idx in UpdateSelectedZoneUi system
    Issue #20

--- src/resources/selected_zone.rs
+++ src/resources/selected_zone.rs
@@ -1,4 +1,2 @@
-use crate::settings::zones_settings::ZoneId;
-
 #[derive(Default)]
 pub struct SelectedZone(pub Option<usize>);
--- src/systems/update_selected_zone_ui.rs
+++ src/systems/update_selected_zone_ui.rs
@@ -4,9 +4,7 @@ use deathframe::amethyst::ui::{UiText, UiTransform};
 const UI_SELECTED_ZONE_ID: &str = "selected_zone";
 
 #[derive(Default)]
-pub struct UpdateSelectedZoneUi {
-    last_selected_zone_idx: Option<usize>,
-}
+pub struct UpdateSelectedZoneUi;
 
 impl<'a> System<'a> for UpdateSelectedZoneUi {
     type SystemData = (
@@ -26,28 +24,20 @@ impl<'a> System<'a> for UpdateSelectedZoneUi {
         ): Self::SystemData,
     ) {
         if let Some(&selected_zone_idx) = selected_zone.0.as_ref() {
-            if self
-                .last_selected_zone_idx
-                .as_ref()
-                .map(|last_idx| *last_idx != selected_zone_idx)
-                .unwrap_or(true)
+            if let Some(selected_zone) =
+                settings.config.zone_order.get(selected_zone_idx)
             {
-                self.last_selected_zone_idx = Some(selected_zone_idx);
-                if let Some(selected_zone) =
-                    settings.config.zone_order.get(selected_zone_idx)
+                for ui_text in (&ui_transform_store, &mut ui_text_store)
+                    .join()
+                    .filter_map(|(transform, text)| {
+                        if &transform.id == UI_SELECTED_ZONE_ID {
+                            Some(text)
+                        } else {
+                            None
+                        }
+                    })
                 {
-                    for ui_text in (&ui_transform_store, &mut ui_text_store)
-                        .join()
-                        .filter_map(|(transform, text)| {
-                            if &transform.id == UI_SELECTED_ZONE_ID {
-                                Some(text)
-                            } else {
-                                None
-                            }
-                        })
-                    {
-                        ui_text.text = selected_zone.to_string();
-                    }
+                    ui_text.text = selected_zone.to_string();
                 }
             }
         }
